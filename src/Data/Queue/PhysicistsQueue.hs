c2f097d58363c3e8136a53bb9a3da862a5498ec5
John Tasto <5026892+JohnTasto@users.noreply.github.com>
2020-07-23 20:37:47 -0700

    Clean up and notate PhysicistsQueue

--- src/Data/Queue/PhysicistsQueue.hs
+++ src/Data/Queue/PhysicistsQueue.hs
@@ -9,9 +9,9 @@ import Data.Queue.Queue
 data PhysicistsQueue a = PQ [a] Int [a] Int [a]
 
 check :: [a] -> Int -> [a] -> Int -> [a] -> PhysicistsQueue a
-check w lenf f lenr r = if lenr <= lenf
-  then checkw w lenf f lenr r
-  else checkw f (lenf + lenr) (f ++ reverse r) 0 []
+check w lenf f lenr r = if lenf < lenr
+  then checkw f (lenf + lenr) (f ++ reverse r) 0 []
+  else checkw w lenf f lenr r
 
 checkw :: [a] -> Int -> [a] -> Int -> [a] -> PhysicistsQueue a
 checkw [] lenf f lenr r = PQ f lenf f lenr r
@@ -20,12 +20,15 @@ checkw w  lenf f lenr r = PQ w lenf f lenr r
 instance Queue PhysicistsQueue where
   empty = PQ [] 0 [] 0 []
 
-  isEmpty (PQ _ lenf _ _ _) = (lenf == 0)
+  isEmpty (PQ _ lenf _ _ _) = lenf == 0
 
+  -- O(1) amortized persistent
   snoc (PQ w lenf f lenr r) x = check w lenf f (lenr + 1) (x:r)
 
+  -- O(1)
   head (PQ []    _ _ _ _) = error "Empty queue"
-  head (PQ (x:_) _ _ _ _) = x
+  head (PQ (n:_) _ _ _ _) = n
 
+  -- O(1) amortized persistent
   tail (PQ []    _    _ _    _) = error "Empty queue"
   tail (PQ (_:w) lenf f lenr r) = check w (lenf - 1) (Data.List.tail f) lenr r
